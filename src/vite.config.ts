cc9f78f7e96b7b9a23ffe204eb40c00b1a1f1955
Moritz <moritzgruss@hotmail.se>
2025-02-15 18:27:13 +0100

    changed repo name in vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/MyWebsite/", // Replace this with your actual repository name
+  base: "/knowledge-pool/", // Replace this with your actual repository name
   server: {
     host: "::",
     port: 8080,
